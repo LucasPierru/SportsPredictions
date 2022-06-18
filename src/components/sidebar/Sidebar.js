55264e134e986af2efaa6e6206d9c27214086036
LucasPierru <lucaspierru7@gmail.com>
2022-06-18 15:55:06 -0400

    Dropdown + correction to link in drop to do

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -15,11 +15,14 @@ function Sidebar() {
                 {menuItems.map((item, index) => {
                         return (
                             <li key={index}>
-                                <div className={"sidebar-listItem"}>
-                                    <SidebarLink isOpen={isOpen} icon={item.icon} route={item.route} routeName={item.title} subItem={item.subItem}/>     
-                                </div> 
-                            </li>  
-                            
+                                <SidebarLink 
+                                    isOpen={isOpen} 
+                                    icon={item.icon} 
+                                    route={item.route} 
+                                    routeName={item.title} 
+                                    subItem={item.subItem}
+                                />     
+                            </li>   
                         )  
                     }   
                 )}
--- src/components/sidebar/SidebarLink.js
+++ src/components/sidebar/SidebarLink.js
@@ -2,43 +2,40 @@ import React, { useState } from 'react';
 import { NavLink } from "react-router-dom";
 import './SidebarLink.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-//import { faAngleDown, faAngleUp } from "@fortawesome/free-solid-svg-icons";
+import { faAngleDown, faAngleUp } from "@fortawesome/free-solid-svg-icons";
 
 function SidebarLink({ isOpen, route, routeName, icon, subItem }) {
     const name = routeName
     const linkActiveClass = isOpen ?"link active" : "link active close" 
     const linkClass = isOpen ? "link" : "link close" 
     const linkIconClass = "link-icon"
+    const linkArrowClass = isOpen ? "link-icon" : "link-icon close"
     const linkTextClass = isOpen ? "link-text" : "link-text close" 
-    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-    const drawerClass = isOpen ? 'drawer-container' : 'drawer-container close'
+    const dropDownClass = isOpen ? "dropDown-link" : "dropDown-link close"
+    const [isDropdownOpen, setDropdownOpen] = useState(false);
+    const showDropdown = () => setDropdownOpen(!isDropdownOpen);
 
     return(
-        <div onMouseLeave={() => setIsDrawerOpen(false)} className='container'>
-            {console.log(isDrawerOpen)}
-            <div className="link-container" onMouseOver={() => setIsDrawerOpen(true)} >
-                <NavLink className={({ isActive }) => isActive ? linkActiveClass : linkClass} to={route}>
-                    <FontAwesomeIcon className={linkIconClass} icon={icon}/> 
-                    <span className={linkTextClass}>{name}</span>
-                </NavLink>   
-            </div> 
-  
-            <div className={drawerClass}>
-                <ul>
-                    {isDrawerOpen && subItem && subItem.map((item, index) => {
-                        return (
-                            <li key={index}>
-                                <div>
-                                    <NavLink className={({ isActive }) => isActive ? linkActiveClass : linkClass} to={item.route}>
-                                        <FontAwesomeIcon className={linkIconClass} icon={item.icon}/> 
-                                        <span className={linkTextClass}>{item.title}</span>
-                                    </NavLink>   
-                                </div>    
-                            </li>
-                        )
-                    })}     
-                </ul>     
-            </div>         
+        <div>
+            <NavLink className={({ isActive }) => isActive ? linkActiveClass : linkClass} to={route} onClick={subItem && showDropdown}>
+                <FontAwesomeIcon width='18px' height='18px' className={linkIconClass} icon={icon}/> 
+                <span className={linkTextClass}>{name}</span>
+                <div className={linkArrowClass}>
+                    {subItem && isDropdownOpen && isOpen
+                    ? <FontAwesomeIcon icon={faAngleUp}/>
+                    : subItem && isOpen
+                    ? <FontAwesomeIcon icon={faAngleDown}/>
+                    : null}
+                </div>
+            </NavLink>   
+            {isDropdownOpen && subItem && subItem.map((item) => {
+                return (
+                    <NavLink className={dropDownClass} to={item.route}>
+                        <FontAwesomeIcon className={linkArrowClass} icon={item.icon}/> 
+                        <span id='dropSpan' className={linkTextClass}>{item.title}</span>
+                    </NavLink>      
+                )
+            })}           
         </div>
         
 
